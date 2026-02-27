337c03d739377d8fbf65cd0849cb1b131aade630
Kaizad Patel <50244730+kaizadp@users.noreply.github.com>
2026-02-26 21:43:33 -0800

    update theme. v1, v2, v3 of maps

--- map-figures/map.R
+++ map-figures/map.R
@@ -6,6 +6,33 @@ library(ggspatial) # for north arrow
 
 
 
+
+theme_kp <- function() {  # this for all the elements common across plots
+  theme_bw() %+replace%
+    theme(legend.position = "top",
+          legend.key=element_blank(),
+          legend.title = element_text(size = 14, hjust = 0),
+          legend.text = element_text(size = 12),
+          legend.key.size = unit(1.5, 'lines'),
+          legend.background = element_rect(colour = NA),
+          panel.border = element_rect(color="black",linewidth=1, fill = NA),
+          
+          plot.title = element_text(hjust = 0, size = 14),
+          axis.text = element_text(size = 14, color = "black"),
+          axis.title = element_text(size = 14, face = "bold", color = "black"),
+          
+          # formatting for facets
+          panel.background = element_blank(),
+          strip.background = element_rect(colour="white", fill="white"), #facet formatting
+          panel.spacing.x = unit(1, "lines"), #facet spacing for x axis
+          panel.spacing.y = unit(1.5, "lines"), #facet spacing for x axis
+          strip.text.x = element_text(size=14, face="bold"), #facet labels
+          strip.text.y = element_text(size=14, face="bold", angle = 270) #facet labels
+    )
+}
+theme_set(theme_kp())
+
+
 #
 ###################################
 ###################################
@@ -126,6 +153,7 @@ base_plot +
     xmin = -75, xmax = -65, ymin = 35, ymax = 43)
 
 # save figure 1300 x 950
+ggsave("map_v1.png", height = 9.5, width = 13)
 
 
 
@@ -260,130 +288,116 @@ ggsave("map_v2.png", height = 9.5, width = 13)
 ###################################
 
 
-# MAP - EXCHANGE-D --------------------------------------------------------
+# MAP V3 ------------------------------------------------------------------
 
-## Creating a map with EXCHANGE-D sites
-## KFP, 2025-05-20
+library(ggplot2)
+library(sf)
+library(rnaturalearth)
+library(rnaturalearthdata)
 
+# 1. Get map data
+countries <- ne_countries(scale = "medium", returnclass = "sf")
+lakes <- ne_download(scale = "medium", type = "lakes", category = "physical", returnclass = "sf")
+great_lakes <- st_crop(lakes, xmin = -95, xmax = -75, ymin = 40, ymax = 50)
 
-library(tidyverse)
-library(sf) # for map
-library(ggspatial) # for north arrow
-
-
-# 
-# 2. Map data ---- 
-
-## Set CRS (coordinate reference system)
-common_crs <- 4326
-
-## Import shapefiles
-## downloaded from: 
-  # Great Lakes: https://www.sciencebase.gov/catalog/item/530f8a0ee4b0e7e46bd300dd
-  # USA: https://www2.census.gov/geo/tiger/TIGER2024/STATE/
-  # the USA shp includes only the US land border, but we also want the Great Lakes
-  # so we're adding the individual GL shp files.
-
-## Set regional and WLE/CB (inset) bounding boxes
-us_bbox <- c(xmin = -125, xmax = -60, ymin = 20, ymax = 50)
-# region_bbox <- c(xmin = -98, xmax = -60, ymin = 32, ymax = 50)
-# cb_bbox <- c(xmin = -78, xmax = -74, ymin = 36.8, ymax = 40.5)
-# wle_bbox <- c(xmin = -93, xmax = -80, ymin = 38, ymax = 48)
-
-greatlakes <- 
-  read_sf("map_shpfiles/greatlakes_subbasins/greatlakes_subbasins.shp") %>% 
-  st_transform(., crs = common_crs) 
-
-us <- 
-  read_sf("map_shpfiles/cb_2024_us_region_5m/cb_2024_us_region_5m.shp") %>% 
-  st_transform(., crs = common_crs) %>% 
-  st_crop(., y = us_bbox)
-
-gl_erie <- read_sf("map_shpfiles/hydro_p_LakeErie/hydro_p_LakeErie.shp") %>% st_transform(., crs = common_crs) 
-gl_huron <- read_sf("map_shpfiles/hydro_p_LakeHuron/hydro_p_LakeHuron.shp") %>% st_transform(., crs = common_crs) 
-gl_michigan <- read_sf("map_shpfiles/hydro_p_LakeMichigan/hydro_p_LakeMichigan.shp") %>% st_transform(., crs = common_crs) 
-gl_ontario <- read_sf("map_shpfiles/hydro_p_LakeOntario/hydro_p_LakeOntario.shp") %>% st_transform(., crs = common_crs) 
-gl_superior <- read_sf("map_shpfiles/hydro_p_LakeSuperior/hydro_p_LakeSuperior.shp") %>% st_transform(., crs = common_crs) 
+usa_countries <- ne_states(returnclass = "sf", country = "United States of America")
+canada_countries <- ne_states(returnclass = "sf", country = "Canada")
 
 
-#
-# 3. Plot map with sites ----
+## create a df with site coordinates and labels
+sites = data.frame(
+  y = c(41.61524, 41.50148, 41.37618,
+        38.43085, 37.21927, 38.874445),
+  x = c(-83.22889, -83.04611, -82.50685,
+        -76.22622, -76.40851, -76.551667),
+  region = c("Erie", "Erie", "Erie",
+             "Chesapeake", "Chesapeake", "Chesapeake"),
+  site = c("CRC", "PTR", "OWC",
+           "MSM", "GWI", "GCW"),
+  label = c("1", "2", "3", 
+            "5", "6", "4"))
 
-base = 
+## Make the base map
+base_plot <- 
+  ggplot() +
+  geom_sf(data = great_lakes, fill = "#56cfe1", alpha = 0.5, color = "black") +
+  geom_sf(data = canada_countries, fill = "grey96") +
+  geom_sf(data = usa_countries) +
+  coord_sf(xlim = c(-95, -55), ylim = c(30, 50), expand = FALSE) +
+  theme_minimal() +
+  theme(panel.background = element_rect(fill = alpha("#48bfe3", 0.5)))+
+  geom_point(
+    data = sites, aes(x, y),
+    size = 2, color = "black")+
+  # CB inset rectangle
+  geom_rect(aes(xmin = -77.8, xmax = -74.5, ymin = 36.8, ymax = 40.5), 
+            fill = NA, color = "black", lwd = 0.75) +
+  geom_segment(aes(x = -74.5, xend = -70, y = 38, yend = 38), 
+               color = "black", lwd = 0.75) + 
+  # WLE inset rectangle
+  geom_rect(aes(xmin = -84, xmax = -82, ymin = 41, ymax = 42), 
+            fill = NA, color = "black", lwd = 0.75) +
+  geom_segment(aes(x = -83, xend = -83, y = 42, yend = 44), 
+               color = "black", lwd = 0.75) + 
+  annotation_north_arrow(location = "tr", which_north = "true", 
+                         pad_x = unit(0.0, "in"), pad_y = unit(0.2, "in"),
+                         style = north_arrow_fancy_orienteering)+
+  #   xlim(-92, -67)+
+  #    ylim(35, 49)+
+  
+  coord_sf(ylim = c(35, 50),
+           xlim = c(-67, -92),
+           expand = TRUE)+
+  
+  theme(panel.border = element_rect(color = "black", fill = NA, size = 1.5),
+        legend.background = element_rect(fill = alpha("white", 0.0)), 
+        legend.key = element_rect(fill = "transparent"), 
+        legend.position = c(0.85, 0.1)) + 
+  labs(x = "", y = "")+
+  NULL
+
+#  us_bbox <- c(xmin = -125, xmax = -60, ymin = 20, ymax = 50)
+
+
+## Make the inset map with just WLE sites
+inset_plot_wle <- 
   ggplot() + 
-  geom_sf(data = us, fill = "grey80", color = "grey80") + 
-  geom_sf(data = gl_erie, fill = "#56cfe1", color = NA) +
-  geom_sf( fill = "#56cfe1", color = NA, data = gl_huron) + 
-  geom_sf( fill = "#56cfe1", color = NA, data = gl_michigan) + 
-  geom_sf( fill = "#56cfe1", color = NA, data = gl_ontario) + 
-  geom_sf( fill = "#56cfe1", color = NA, data = gl_superior)+ 
-  theme(
-        legend.position = "none",
-        panel.grid = element_blank(),
+  geom_sf(data = wle_states) + 
+  geom_point(
+    data = sites %>% filter(region == "Erie"), aes(x, y),
+    size = 5, color = "black")+
+  geom_text(data = sites %>% filter(region == "Erie"), aes(x, y, label = label), nudge_x = 0, nudge_y = -0.10, size = 7)+
+  scale_x_continuous(expand = c(0,0))+
+  scale_y_continuous(expand = c(0,0))+
+  cowplot::theme_map() + 
+  theme(legend.position = "none",
+        panel.background = element_rect(fill = "#56cfe1"),
         panel.border = element_rect(color = "black", fill = NA, size = 1.5),
-        axis.text = element_blank(),
-        axis.ticks = element_blank()) + 
-  labs(x = "", y = "")+
-  NULL
+        axis.text = element_blank()) 
 
-## crop map to each region
-
-gl = 
-  base + 
-  coord_sf(ylim = c(40, 50),
-           xlim = c(-76.5, -92),
-           expand = TRUE)+
-  theme(panel.background = element_rect(fill = "grey95")) 
-
-cb = 
-  base + 
-  coord_sf(ylim = c(36, 40),
-           xlim = c(-74, -78),
-           expand = TRUE)+
-  theme(panel.background = element_rect(fill = "#48bfe3")) 
-
-
-cowplot::plot_grid(gl, cb, align = "hv")
-# exported 1200 * 800
-ggsave("ec-d_map_sampled_sites_2025-09-05.jpg", height = 8, width = 12)
-
-# 
-#
-# 3. Plot blank map ----
-
-base = 
+## Make the inset map with just CB sites
+inset_plot_cb <- 
   ggplot() + 
-  geom_sf(data = us, fill = "grey80", color = "grey80") + 
-  geom_sf(data = gl_erie, fill = "#56cfe1", color = NA) +
-  geom_sf( fill = "#56cfe1", color = NA, data = gl_huron) + 
-  geom_sf( fill = "#56cfe1", color = NA, data = gl_michigan) + 
-  geom_sf( fill = "#56cfe1", color = NA, data = gl_ontario) + 
-  geom_sf( fill = "#56cfe1", color = NA, data = gl_superior)+ 
-  theme(
-    legend.position = "none",
-    panel.grid = element_blank(),
-    panel.border = element_rect(color = "black", fill = NA, size = 1.5),
-    axis.text = element_blank(),
-    axis.ticks = element_blank()) + 
-  labs(x = "", y = "")+
-  NULL
+  geom_sf(data = cb_states) + 
+  geom_point(
+    data = sites %>% filter(region == "Chesapeake"), aes(x, y),
+    size = 5, color = "black")+
+  geom_text(data = sites %>% filter(region == "Chesapeake"), aes(x, y, label = label), nudge_x = -0.25, nudge_y = -0.00, size = 7)+
+  scale_x_continuous(expand = c(0,0))+
+  scale_y_continuous(expand = c(0,0))+
+  cowplot::theme_map() + 
+  theme(legend.position = "none",
+        panel.background = element_rect(fill = "#48bfe3"),
+        panel.border = element_rect(color = "black", fill = NA, size = 1.5),
+        axis.text = element_blank()) 
 
-## crop map to each region
+## Combine into single figure
+base_plot + 
+  annotation_custom(
+    ggplotGrob(inset_plot_wle), 
+    xmin = -90, xmax = -80, ymin = 42, ymax = 50)+
+  annotation_custom(
+    ggplotGrob(inset_plot_cb), 
+    xmin = -75, xmax = -65, ymin = 35, ymax = 43)
 
-gl = 
-  base + 
-  coord_sf(ylim = c(40, 50),
-           xlim = c(-76.5, -92),
-           expand = TRUE)+
-  theme(panel.background = element_rect(fill = "grey95")) 
-
-cb = 
-  base + 
-  coord_sf(ylim = c(36, 40),
-           xlim = c(-74, -78),
-           expand = TRUE)+
-  theme(panel.background = element_rect(fill = "#48bfe3")) 
-
-
-cowplot::plot_grid(gl, cb, align = "hv")
-# exported 1200 * 800  
+ggsave("map_v3.png", height = 9.5, width = 13)
